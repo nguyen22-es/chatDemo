8a5bf51c0746e87fab123772ef99e98be45613c7
nguyen22-es <112994817+nguyen22-es@users.noreply.github.com>
2024-04-17 17:11:35 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using chatDeMo.Models;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Practical.AspNetCore.SignalR;
+using System.Diagnostics;
+
+namespace chatDeMo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IHubContext<MessageHub> _hubContext;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, IHubContext<MessageHub> hubContext)
+        {
+            _logger = logger;
+            _hubContext = hubContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+
+
+        [HttpPost("/announcement")]
+        public async Task<IActionResult> Post([FromForm] string message)
+        {
+            await _hubContext.Clients.All.SendAsync("ReceiveMessage", message);
+            return RedirectToAction("Index");
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
